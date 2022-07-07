e989818cf1818045b6fc274a07b6eeb81230fce6
jrblanco <jr.cosio1@gmail.com>
2022-07-07 23:23:25 +0200

    Terminado el ejemplo

---
+++ README.md
@@ -0,0 +1,3 @@
+# BotonEnMenuNavigationView
+
+Este es un ejemplo de como integrar el botón flotante en el menu NavigationView
---
+++ app/src/main/java/com/jrblanco/botonenmenunavigationview/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.jrblanco.botonenmenunavigationview
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var bottomNavigationView:BottomNavigationView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        bottomNavigationView = findViewById(R.id.bottomNavigationView)
+
+        bottomNavigationView.background = null
+    }
+}
